ad53a2e137d2769553609d3af32d601891a5a3b0
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-10 22:08:03 +0100

    Initial commit

---
+++ src/main/java/com/example/remitly/RemitlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.remitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemitlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/remitly/RemitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
