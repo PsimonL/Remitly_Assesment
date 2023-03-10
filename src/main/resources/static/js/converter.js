98c1fff9ccce219e5b34deae746ebadf4779433a
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-10 23:32:42 +0100

    js script

---
+++ src/main/resources/static/js/converter.js
@@ -0,0 +1,17 @@
+const dict = {
+    "CH": "chf",
+    "PL": "pln",
+    "EU": "eur",
+    "GB": "gbp",
+    "US": "usd"
+};
+
+const pickedCurrency1 = document.getElementById('selector1');
+const pickedCurrency2 = document.getElementById('selector2');
+const valueForInput = document.getElementById('enter');
+const valueForOutput = document.getElementById('conversion');
+const conversionButton =  document.getElementById('exchange-button');
+const outputRatio = document.getElementById('conversion');
+
+const select1 = document.getElementById('selector1');
+const select2 = document.getElementById('selector2');
