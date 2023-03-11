caa74c66d77d90beaa17387e04a454900866990e
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-11 15:46:16 +0100

    fixes

--- src/main/java/com/example/remitly/controller/UserController.java
+++ src/main/java/com/example/remitly/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.remitly.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import static java.lang.invoke.VarHandle.AccessMode.GET;
+
+@Controller
 public class UserController {
-    @GetMapping("/")
-    public String singlePage() {
-        return "single-page";
+    @RequestMapping(value = "/converter")
+    public String converter_page() {
+        System.out.println("working");
+        return "converter";
     }
 }
