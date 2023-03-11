72a0487d6dd3f5fa09aacf493e6197b0374dd54c
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-11 15:02:03 +0100

    js script

--- src/main/resources/static/js/converter.js
+++ src/main/resources/static/js/converter.js
@@ -15,3 +15,23 @@ const outputRatio = document.getElementById('conversion');
 
 const select1 = document.getElementById('selector1');
 const select2 = document.getElementById('selector2');
+
+let fromCurr = [];
+let toCurr = [];
+function controlFlag(element, flagId) {
+    element.addEventListener('change', function () {
+        const selectedCountry = element.value;
+        // console.log("selectedCountry = " + selectedCountry);
+        if (flagId === "flag1") {
+            fromCurr.push(selectedCountry);
+        }
+        if(flagId === "flag2"){
+            toCurr.push(selectedCountry)
+        }
+        const apiUrl = 'https://www.countryflagicons.com/FLAT/64/' + selectedCountry + '.png';
+        const flagImage = document.getElementById(flagId);
+        flagImage.src = apiUrl;
+    });
+}
+controlFlag(select1, 'flag1');
+controlFlag(select2, 'flag2');
