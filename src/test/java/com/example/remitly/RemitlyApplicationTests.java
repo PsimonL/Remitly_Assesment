aba6a8127541ec791995464f827e3c1808e9c024
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-12 23:46:08 +0100

    finished

--- src/test/java/com/example/remitly/RemitlyApplicationTests.java
+++ src/test/java/com/example/remitly/RemitlyApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.remitly;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -11,4 +14,16 @@ class RemitlyApplicationTests {
         System.out.println("Hello World!");
     }
 
+    @Test
+    void work() {
+        String chromeDriverPath = "src/test/java/com/example/remitly/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8081");
+        String pageTitle = driver.getTitle();
+        System.out.println("Page title is: " + pageTitle);
+        driver.quit();
+    }
 }
